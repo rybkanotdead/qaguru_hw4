67017d56508f4421b7d0cd049dea1cb1255335bc
Alina Oganina <ifoxspirit@yandex.ru>
2025-02-16 10:57:46 +0500

    добавила загрузку файла и поправила выбор гендера, хобби и день рождения

--- test/testtodo.py
+++ test/testtodo.py
@@ -1,13 +1,15 @@
 from selene import browser, have, be
-
+import os
 
 def test_form():
     browser.open('/automation-practice-form')
+    browser.driver.execute_script("$('#fixedban').remove()")
+    browser.driver.execute_script("$('footer').remove()")
 
     browser.element('#firstName').type('Alina')
     browser.element('#lastName').type('Oga')
     browser.element('#userEmail').type('alina.oga@alina.oga')
-    browser.element('[for="gender-radio-2"]').click()
+    browser.element('//label[contains(text(), "Female")]').click()
     browser.element('#userNumber').type('8800353535')
 
     browser.element('#dateOfBirthInput').click()
@@ -17,11 +19,11 @@ def test_form():
     browser.element('.react-datepicker__year-select').click().element(
         'option[value="1999"]'
     ).click()
-    browser.element('.react-datepicker__day--026').click()
+    browser.element('//div[contains(@class, "react-datepicker__day") and text()="26"]').click()
 
     browser.element('#subjectsInput').type('Computer Science').press_enter()
-    browser.element('[for="hobbies-checkbox-3"]').click()
-
+    browser.element('//label[contains(text(), "Music")]').should(be.visible).click()
+    browser.element('#uploadPicture').type(f"{os.path.dirname(os.getcwd())}/picture/pic1.jpg")
     browser.element('#currentAddress').type('Abaya 26A')
     browser.element('#state').click().element(
         'div[id^="react-select-3-option"]'
@@ -29,7 +31,9 @@ def test_form():
     browser.element('#city').click().element('div[id^="react-select-4-option"]').click()
 
     browser.element('#submit').press_enter()
+
     browser.element('.modal-content').should(be.visible)
+
     browser.element('.table').should(have.text('Alina Oga'))
     browser.element('.table').should(have.text('alina.oga@alina.oga'))
     browser.element('.table').should(have.text('Female'))
@@ -37,5 +41,6 @@ def test_form():
     browser.element('.table').should(have.text('26 September,1999'))
     browser.element('.table').should(have.text('Computer Science'))
     browser.element('.table').should(have.text('Music'))
+    browser.element('.table').should(have.text('pic1.jpg'))
     browser.element('.table').should(have.text('Abaya 26A'))
     browser.element('.table').should(have.text('NCR Delhi'))
